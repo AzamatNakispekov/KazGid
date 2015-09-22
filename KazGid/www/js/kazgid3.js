83a9adde0dd4bf2e075d4c8c05e0ad36e2c950c0
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-09-23 00:43:39 +0600

    скрипт
    скрипт

--- KazGid/www/js/kazgid3.js
+++ KazGid/www/js/kazgid3.js
@@ -28,19 +28,19 @@ console.log(decodeSection0(['14382', '19081']));
 
 function decodeGroup11(data){
     return {
-    IdForH8: data[0].substring(0, 1),
-    H8: data[0].substring(2, 5)
+    IdForH8: data.substring(0, 1),
+    H8: data.substring(2, 5)
     }
 
 }
     
 function decodeGroup12(data){
-    var n1 = parseInt(data[0].substring(4, 5));
+    var n1 = parseInt(data.substring(4, 5));
  
-    var change = parseInt(data[0].substring(2, 4));
+    var Change = parseInt(data.substring(2, 4));
     return {
-    idforCha: parseInt(data[0].substring(0, 1)),
-    change: change,
+    IdForHCha: parseInt(data.substring(0, 1)),
+    Change: Change,
     n1: n1,
     hasSection1: n1 === 1 || n1 === 2 
     } 
@@ -48,21 +48,21 @@ function decodeGroup12(data){
 
 function decodeGroup13(data){
     return {
-    idforH20: data[0].substring(0,1),
-    H20:data[0].substring(2,5),}}
+    IdForH20: data.substring(0,1),
+    H20:data.substring(2,5),}}
     
 function decodeGroup14(data){
     return{
-    WaterAndAirTid: data[0].substring(0,1),
-    WaterTem:data[0].substring(1,2),
-    AirTemp:data[0].substring(4,5),
+    WaterAndAirTid: data.substring(0,1),
+    TempOfWater:data.substring(1,2),
+    TempOfAir:data.substring(4,5),
     }
 }
 
 function decodeGroup15 (data){
     return{
-    TheStateOfThePondId:data[0].substring(0,1),
-    TheStateOfThePond:data[0].substring(1,5),
+    TheStateOfThePondId:data.substring(0,1),
+    TheStateOfThePond:data.substring(1,5),
 
     }
         
@@ -70,47 +70,50 @@ function decodeGroup15 (data){
  
 console.log('decode 10369 20102 30368 44003');
 
-console.log(decodeGroup11(['10369']));
+console.log(decodeGroup11('10221'));
 
-console.log(decodeGroup12(['20102']));
+console.log(decodeGroup12('20022'));
 
-console.log(decodeGroup13(['30368']));
+console.log(decodeGroup13('30223'));
 
-console.log(decodeGroup14(['44003']));
+console.log(decodeGroup14('47615'));
+
+console.log(decodeGroup15('60000'));
+
 
 console.log('decode 10221 20022 30223 47615');
 
 function decodeSection1(data) {
     
-    var i  = decodeGroup11(data)
+    var group1  = decodeGroup11(data)
   
-    var i1 = decodeGroup12(data)
+    var group2 = decodeGroup12(data)
  
-    var i2 = decodeGroup13(data)
+    var group3 = decodeGroup13(data)
   
-    var i3 = decodeGroup14(data)
+    var group4 = decodeGroup14(data)
  
-    var i4 = decodeGroup15(data)
+    var group5 = decodeGroup15(data)
  
     return {
-    IdForH8: data[0].substring(0, 1),
-    H8: data[0].substring(2, 5),
+    IdForH8: group1.IdForH8,
+    H8: group1.H8,
     
-    IdForHCha: parseInt(data[1].substring(0, 1)),
-    Change: change,
-    n1: n1,
-    hasSection1: n1 === 1 || n1 === 2 ,
+    IdForHCha: group2.IdForHCha,
+    Change: group2.Change,
+    n1: group2.n1,
+    hasSection1: group2.hasSection1 ,
     
-    IdForH20: data[2].substring(0,1),
-    H20:data[2].substring(2,5),
+    IdForH20: group3.IdForH20,
+    H20:group3.H20,
      
-    WaterAndAirTid: data[3].substring(0,1),
-    TempOfWater:data[3].substring(1,2),
-    TempOfAir:data[3].substring(4,5),
+    WaterAndAirTid: group4.WaterAndAirTid,
+    TempOfWater: group4.TempOfWater,
+    TempOfAir:group4.TempOfAir,
     
-    TheStateOfThePondId: data[4].substring(0,1),
-    TheStateOfThePond:data[4].substring(1,5),
+    TheStateOfThePondId: group5.TheStateOfThePondId,
+    TheStateOfThePond: group5.TheStateOfThePond,
 }
  }
-console.log(decodeSection1(['10221', '20022', '30223', '47615','60000']));
+console.log(decodeSection1('10221', '20022', '30223', '47615','60000'));
 
