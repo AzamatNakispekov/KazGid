b7557cdcd50278610cac7afc60d3646afb9b4ecb
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-09-22 23:27:10 +0600

    новая замена табуляция на 4 пробела

--- KazGid/www/js/kazgid3.js
+++ KazGid/www/js/kazgid3.js
@@ -1,24 +1,24 @@
 function decodeSection0(data) {
-	if (data.length !== 2 && data.length !== 3) {
-		throw new Error("Please pass array which consists from 2 or 3, 5 character code sequences.");
-	}
-	
-	if (data.length === 3) {
-		throw new Error("The information about hydorlogic post is not supported.");
-	}
-	
-	var n = parseInt(data[1].substring(4, 5));
-	var term = parseInt(data[1].substring(2, 4));
-	return {
-		riverBasin: data[0].substring(0, 2),
-		postNo: data[0].substring(2, 5),
-		day: parseInt(data[1].substring(0, 2)),
-		term: term,
-		n: n,
-		hasSection1: n === 1 || n === 2 || (n === 3 && term !== 8),
-		hasOtherSections: (n === 2 && term === 8) || (n === 4 && term === 8) || (n === 5 && term === 8), 
-		hasSection7: n === 7
-	}
+    if (data.length !== 2 && data.length !== 3) {
+        throw new Error("Please pass array which consists from 2 or 3, 5 character code sequences.");
+    }
+    
+    if (data.length === 3) {
+        throw new Error("The information about hydorlogic post is not supported.");
+    }
+    
+    var n = parseInt(data[1].substring(4, 5));
+    var term = parseInt(data[1].substring(2, 4));
+    return {
+        riverBasin: data[0].substring(0, 2),
+        postNo: data[0].substring(2, 5),
+        day: parseInt(data[1].substring(0, 2)),
+        term: term,
+        n: n,
+        hasSection1: n === 1 || n === 2 || (n === 3 && term !== 8),
+        hasOtherSections: (n === 2 && term === 8) || (n === 4 && term === 8) || (n === 5 && term === 8), 
+        hasSection7: n === 7
+    }
     
 }
 console.log("Decode Section 0: 14382 19081");
@@ -27,20 +27,21 @@ console.log(decodeSection0(['14382', '19081']));
 
 
 function decodeGroup11(data){
-  return {
+    return {
         IdForH8: data[0].substring(0, 1),
-		H8: data[0].substring(2, 5)
+        H8: data[0].substring(2, 5)
    }
 
 }
     
 function decodeGroup12(data){
     var n1 = parseInt(data[0].substring(4, 5));
+ 
     var change = parseInt(data[0].substring(2, 4));
     return {
         idforCha: parseInt(data[0].substring(0, 1)),
-		change: change,
-		n1: n1,
+        change: change,
+        n1: n1,
         hasSection1: n1 === 1 || n1 === 2 
     } 
 }
@@ -52,7 +53,7 @@ function decodeGroup13(data){
     
 function decodeGroup14(data){
     return{
-	    WaterAndAirTid: data[0].substring(0,1),
+        WaterAndAirTid: data[0].substring(0,1),
         WaterTem:data[0].substring(1,2),
         AirTemp:data[0].substring(4,5),
     }
@@ -95,15 +96,12 @@ function decodeSection1(data) {
        IdForH8: data[0].substring(0, 1),
        H8: data[0].substring(2, 5),
    
-    
        IdForHCha: parseInt(data[1].substring(0, 1)),
        Change: change,
        n1: n1,
        hasSection1: n1 === 1 || n1 === 2 ,
        
-       
-    
-       idforH20: data[2].substring(0,1),
+       IdForH20: data[2].substring(0,1),
        H20:data[2].substring(2,5),
       
        WaterAndAirTid: data[3].substring(0,1),
