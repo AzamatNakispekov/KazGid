43acc47e655bf2ce9f8130b66b13d6205747a7ac
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-10-12 13:36:52 +0600

    Исправление поледней функции
    Исправлена последняя функция, массивы теперь получают правильные данные,
    с учетом длины  массива.

--- KazGid/www/js/kazgid3.js
+++ KazGid/www/js/kazgid3.js
@@ -9,16 +9,16 @@ function splitCode(data){
    
      if(data.length>30){
         var intensity=data.substring(30,35);
-        return mass=[post,data1,LevelOfWaterAt8,changes,LevelOfWaterAt20,temperatures,intensity] ;
+        return [post,data1,LevelOfWaterAt8,changes,LevelOfWaterAt20,temperatures,intensity] ;
      }
-return mass=[post,data1,LevelOfWaterAt8,changes,LevelOfWaterAt20,temperatures];
+return [post,data1,LevelOfWaterAt8,changes,LevelOfWaterAt20,temperatures];
 
 }
 
-console.log("Array length", splitCode('123456789012345678901234567890').length); 
-console.log("Array length", splitCode('12345678901234567890123456789000000').length);
-console.log(splitCode('12345678901234567890123456789000000'));
-console.log(splitCode('123456789012345678901234567890'));
+//console.log("Array length", splitCode('123456789012345678901234567890').length); 
+//console.log("Array length", splitCode('12345678901234567890123456789000000').length);
+//console.log(splitCode('12345678901234567890123456789000000'));
+//console.log(splitCode('123456789012345678901234567890'));
 
 function decodeSection0(data) {
     if (data.length !== 2 && data.length !== 3) {
@@ -43,8 +43,8 @@ function decodeSection0(data) {
     }
     
 }
-console.log("Decode Section 0: 14382 19081");
-console.log(decodeSection0(['14382', '19081']));
+//console.log("Decode Section 0: 14382 19081");
+//console.log(decodeSection0(['14382', '19081']));
 
 
 
@@ -90,20 +90,20 @@ function decodeGroup16 (data){
         
 }
  
-console.log('decode 10369 20102 30368 44003');
+//console.log('decode 10369 20102 30368 44003');
+//
+//console.log(decodeGroup11('10221'));
+//
+//console.log(decodeGroup12('20022'));
+//
+//console.log(decodeGroup13('30223'));
 
-console.log(decodeGroup11('10221'));
+//console.log(decodeGroup14('47615'));
 
-console.log(decodeGroup12('20022'));
-
-console.log(decodeGroup13('30223'));
-
-console.log(decodeGroup14('47615'));
-
-console.log(decodeGroup16('60000'));
+//console.log(decodeGroup16('60000'));
 
 
-console.log('decode 10221 20022 30223 47615');
+//console.log('decode 10221 20022 30223 47615');
 
 function decodeSection1(data) {
     
@@ -141,29 +141,35 @@ function decodeSection1(data) {
     TheStateOfThePond: group6.TheStateOfThePond,
 }
  }
-console.log(decodeSection1(['10221', '20022', '30223', '47615','60000']));
-console.log(decodeSection1(['10369', '20102', '30368', '44003']));
-console.log(decodeSection1(['10221', '20022', '30223', '47615']));
+//console.log(decodeSection1(['10221', '20022', '30223', '47615','60000']));
+//console.log(decodeSection1(['10369', '20102', '30368', '44003']));
+//console.log(decodeSection1(['10221', '20022', '30223', '47615']));
 
 
- console.log("Function for decoding");
+console.log("Function for decoding");
+
 function decode(data){
     var CutKode=splitCode(data);
     var newmass1=[CutKode[0],CutKode[1]];
     var newmass2=[CutKode[2],CutKode[3],CutKode[4],CutKode[5]];
     console.log(CutKode);
     console.log(newmass1);
-    console.log(newmass2);
-    console.log("Здесь работает");
-    if(CutKode.lenght>4){
-    var newmass2=[CutKode[2],CutKode[3],CutKode[4],CutKode[5],CutKode[6]];}
-    var decodesec0=decodeSection0(newmass1);
-    var decodesec1=decodeSection1(newmass2);
-    console.log(decodesec0);
-    console.log(decodesec1);}
 
-
-console.log(decode('12332143465324575684576856796859675678576984568'))
+   if(CutKode.length>6)
+   {
+   var newmass2=[CutKode[2],CutKode[3],CutKode[4],CutKode[5],CutKode[6]];
+   }
+  
+   console.log(newmass2);
+    
+   
+   var decodesec0=decodeSection0(newmass1);
+   var decodesec1=decodeSection1(newmass2);
+   
+   console.log(decodesec0);
+   console.log(decodesec1);
+}
+console.log(decode('12345123451234512345123451234512345'));
     
     
     
